97c642494789167ebda3ea7a9d264448c150b5a2
xin <bryt.xin.li@gmail.com>
2024-04-07 16:55:26 +0100

    update

--- scripts/gen-sitemap/main.py
+++ scripts/gen-sitemap/main.py
@@ -1,9 +1,12 @@
+import os
+import glob
 import pathlib
 from post_parser import *
 
 filepath = pathlib.Path(__file__).resolve().parent
 
-recipes = glob.glob("*", root_dir=os.path.join('content', 'recipes')) 
+recipes = glob.glob(os.path.join('content', 'recipes', '*')) 
 for recipe in recipes:
+    recipe = recipe.lstrip(os.path.join('content', 'recipes'))
     parser = PostParser(recipe)
     svg = parser.save_svg()
